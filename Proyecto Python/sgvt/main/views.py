2cfbbea8b04ff4fb0f39b0241b4f193f08d5e1a5
AndDelMar <amar310804@gmail.com>
2024-04-29 12:25:15 -0500

    Primer commit real

---
+++ Proyecto Python/sgvt/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def principal(request):
+    titulo = "Bienvenido"
+    context = {
+        "titulo":titulo,
+    }
+    return render(request, "index.html", context)
