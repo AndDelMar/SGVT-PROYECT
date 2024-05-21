15c5c3e2cd9d566de0b6311c05f1020da8e3ba8b
maria hernandez <maria_dhernandez@soy.sena.edu.co>
2024-05-21 17:17:19 -0500

    eliminacion de la carpeta modelos

--- general/settings.py
+++ general/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',# pip install django-bootstrap5
-    'usuarios'
+    
 ]
 
 MIDDLEWARE = [
