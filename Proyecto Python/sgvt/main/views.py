251ca2705f8312a0fe724cd956be24e5628cf9b5
maria hernandez <maria_dhernandez@soy.sena.edu.co>
2024-05-21 16:45:58 -0500

    cambio de estructura de carpetas y creacion de ambiente de python

--- Proyecto Python/sgvt/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def principal(request):
-    titulo = "Bienvenido"
-    context = {
-        "titulo":titulo,
-    }
-    return render(request, "index.html", context)
---
+++ general/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+def principales(request):
+    titulo ="Bienvenido"
+    context={
+"titulo": titulo,
+    }
+    return render(request, "index.html",context)
